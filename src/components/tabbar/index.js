c57809895710128016738b676eb2ccad105d3228
MarryYou <614345508@qq.com>
2018-12-25 17:46:50 +0800

    change tabbar

--- src/components/tabbar/index.js
+++ src/components/tabbar/index.js
@@ -22,46 +22,44 @@ class TabBarBox extends Component {
   };
   render() {
     return (
-      <footer className="TabBarBox">
-        <TabBar className="TabBar">
-          <TabBarItem
-            active={this.state.tab_active == 0}
-            onClick={() => this.TabChange(0, "/")}
-          >
-            <TabBarIcon className="bar-item">
-              <i className="fa fa-home bar-icon" />
-            </TabBarIcon>
-            <TabBarLabel>首页</TabBarLabel>
-          </TabBarItem>
-          <TabBarItem
-            active={this.state.tab_active == 1}
-            onClick={() => this.TabChange(1, "/rank")}
-          >
-            <TabBarIcon className="bar-item">
-              <i className="fa fa-sort-amount-desc bar-icon" />
-            </TabBarIcon>
-            <TabBarLabel>排行榜</TabBarLabel>
-          </TabBarItem>
-          <TabBarItem
-            active={this.state.tab_active == 2}
-            onClick={() => this.TabChange(2, "/search")}
-          >
-            <TabBarIcon className="bar-item">
-              <i className="fa fa-search bar-icon" />
-            </TabBarIcon>
-            <TabBarLabel>查找</TabBarLabel>
-          </TabBarItem>
-          <TabBarItem
-            active={this.state.tab_active == 3}
-            onClick={() => this.TabChange(3, "/project")}
-          >
-            <TabBarIcon className="bar-item">
-              <i className="fa fa-info-circle bar-icon" />
-            </TabBarIcon>
-            <TabBarLabel>项目介绍</TabBarLabel>
-          </TabBarItem>
-        </TabBar>
-      </footer>
+      <TabBar className="TabBar">
+        <TabBarItem
+          active={this.state.tab_active == 0}
+          onClick={() => this.TabChange(0, "/")}
+        >
+          <TabBarIcon className="bar-item">
+            <i className="fa fa-home bar-icon" />
+          </TabBarIcon>
+          <TabBarLabel>首页</TabBarLabel>
+        </TabBarItem>
+        <TabBarItem
+          active={this.state.tab_active == 1}
+          onClick={() => this.TabChange(1, "/rank")}
+        >
+          <TabBarIcon className="bar-item">
+            <i className="fa fa-sort-amount-desc bar-icon" />
+          </TabBarIcon>
+          <TabBarLabel>排行榜</TabBarLabel>
+        </TabBarItem>
+        <TabBarItem
+          active={this.state.tab_active == 2}
+          onClick={() => this.TabChange(2, "/search")}
+        >
+          <TabBarIcon className="bar-item">
+            <i className="fa fa-search bar-icon" />
+          </TabBarIcon>
+          <TabBarLabel>查找</TabBarLabel>
+        </TabBarItem>
+        <TabBarItem
+          active={this.state.tab_active == 3}
+          onClick={() => this.TabChange(3, "/project")}
+        >
+          <TabBarIcon className="bar-item">
+            <i className="fa fa-info-circle bar-icon" />
+          </TabBarIcon>
+          <TabBarLabel>项目介绍</TabBarLabel>
+        </TabBarItem>
+      </TabBar>
     );
   }
 }
