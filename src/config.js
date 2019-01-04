17e3bf48dce658bc6e1e06c81f7c707772a3ec61
MarryYou <614345508@qq.com>
2019-01-04 18:00:06 +0800

    video route

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// export const domain = "http://59.110.243.107:8086";
-export const domain = "http://localhost:8086";
+export const domain = "http://59.110.243.107:8086";
+// export const domain = "http://localhost:8086";
 export const partitionList = {
   "1": [
     {
