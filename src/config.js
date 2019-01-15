b5d59d66f229bd0f85199593e735444d05b384c5
MarryYou <614345508@qq.com>
2019-01-15 14:24:18 +0800

    video front test

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 // export const domain = "http://59.110.243.107:8086";
-// export const domain = "http://localhost:8086";
-export const domain = "http://0.0.0.0:8086";
+export const domain = "http://localhost:8086";
 export const partitionList = {
   "1": [
     {
